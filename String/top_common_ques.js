d4225b2a8789f4b11086a6b4d9c5e37c8ee763b7
Sumitgiri15434 <sumitgiri15434@gmail.com>
2026-06-03 17:34:39 +0530

    add new program

--- String/top_common_ques.js
+++ String/top_common_ques.js
@@ -6,10 +6,29 @@
 
 // -------- check polindrome ------------
 
-let str = "gog";
-let pali = str.split('').reverse().join('');
-if(str===pali){
-    console.log("palindrome number");
-}else{
-    console.log("not palindrome number");
+// let str = "gog";
+// let pali = str.split('').reverse().join('');
+// if(str===pali){
+//     console.log("palindrome number");
+// }else{
+//     console.log("not palindrome number");
+// }
+
+// ------- count string ----------
+
+let str = 'Sumit Kumar Giri';
+// console.log(str.length);  // count character using space also
+let count=0;
+for(let char of str){
+    if("aeiou".includes(char)){
+        count++;
+    }
 }
+console.log(count);
+
+
+
+
+
+
+
